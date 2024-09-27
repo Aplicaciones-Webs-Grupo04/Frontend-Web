c4be51e6884c823affd021817a9e02fd79394cbb
RominaMaita <u202213765@upc.edu.pe>
2024-09-27 16:26:39 -0500

    feat: added method for getting patients cards.

--- src/medication-management/pages/medication-forms.component.vue
+++ src/medication-management/pages/medication-forms.component.vue
@@ -1,47 +1,49 @@
 <template>
-  <div class="forms-section">
-    <!-- lEFT SIDE FOR THE FORMS -->
-    <div class="form-container">
-      <h5>Medication Name</h5>
-      <pv-chips v-model="name" />
+  <div class="page-container">
+    <div class="forms-section">
+      <!-- lEFT SIDE FOR THE FORMS -->
+      <div class="form-container">
+        <h5>Medication Name</h5>
+        <pv-chips v-model="name" />
 
-      <h5>Dosage/Frequency</h5>
-      <pv-chips v-model="interval" separator="," />
+        <h5>Dosage/Frequency</h5>
+        <pv-chips v-model="interval" separator="," />
 
-      <h5>Quantity</h5>
-      <pv-chips v-model="quantity">
-        <template #chip="slotProps">
-          <div class="chip-template">
-            <span>{{ slotProps.value }} - (active) </span>
-            <i class="pi pi-user-plus" style="font-size: 14px"></i>
-          </div>
-        </template>
-      </pv-chips>
+        <h5>Quantity</h5>
+        <pv-chips v-model="quantity">
+          <template #chip="slotProps">
+            <div class="chip-template">
+              <span>{{ slotProps.value }} - (active) </span>
+              <i class="pi pi-user-plus" style="font-size: 14px"></i>
+            </div>
+          </template>
+        </pv-chips>
 
-      <h5>Started Date</h5>
-        <pv-calendar v-model="startedDate" date-format="dd/mm/yy">
+        <h5>Started Date</h5>
+          <pv-calendar v-model="startedDate" date-format="dd/mm/yy">
+          </pv-calendar>
+
+        <h5>End Date</h5>
+        <pv-calendar v-model="endDate" date-format="dd/mm/yy">
         </pv-calendar>
 
-      <h5>End Date</h5>
-      <pv-calendar v-model="endDate" date-format="dd/mm/yy">
-      </pv-calendar>
-
-      <button class="submit-button" @click="submitForm">Save</button>
+        <button class="submit-button" @click="submitForm">Save</button>
+      </div>
     </div>
-  </div>
 
-  <!-- RIGHT SIDE FOR THE FORM -->
-  <div class="patient-card">
-    <div v-for="medication in medications" :key="medication.id" class="medication-card">
-      <h5>Medication Name: {{ medication.name }}</h5>
-      <p>Dosage/Frequency: {{ medication.interval}}</p>
-      <p>Quantity: {{ medication.quantity }}</p>
-      <p>Start Date: {{medication.startedDate}}</p>
-      <p>End Date: {{medication.endDate}}</p>
+    <!-- RIGHT SIDE FOR THE FORM -->
+    <div class="patient-card">
+      <div v-for="medication in medications" :key="medication.id" class="medication-card">
+        <h5>Medication Name: {{ medication.name }}</h5>
+        <p>Dosage/Frequency: {{ medication.interval}}</p>
+        <p>Quantity: {{ medication.quantity }}</p>
+        <p>Start Date: {{medication.startedDate}}</p>
+        <p>End Date: {{medication.endDate}}</p>
+      </div>
+
     </div>
 
   </div>
-
 </template>
 
 <script>
@@ -89,6 +91,9 @@ export default {
 
     }
 
+  },
+  mounted() {
+    this.getMedications();
   }
 };
 
