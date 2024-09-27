0af11d75433b01973f10042668265561b60714d2
RominaMaita <u202213765@upc.edu.pe>
2024-09-26 21:33:19 -0500

    feat: added medication route.

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import {createRouter, createWebHistory} from "vue-router";
 import HomeComponent from "../public/pages/home.component.vue";
 import AboutComponent from "../public/pages/about.component.vue";
+import MedicationFormsComponent from "../medication-management/pages/medication-forms.component.vue";
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/home', component: HomeComponent, meta: { title: 'Home' } },
         { path: '/about', component: AboutComponent, meta: { title: 'About us' } },
+        { path: '/medication', component: MedicationFormsComponent, meta: { title: 'Medication' } },
         { path: '/', redirect: '/home' },
     ],
 });
