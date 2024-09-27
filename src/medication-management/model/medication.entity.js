23e076def43817cc2f8c66cb1f3b001b02f077c2
RominaMaita <u202213765@upc.edu.pe>
2024-09-26 21:33:41 -0500

    feat: added medication entity variables.

---
+++ src/medication-management/model/medication.entity.js
@@ -0,0 +1,18 @@
+export class Medication {
+    constructor(id, name, description, startDate, endDate, interval, quantity, patientId) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.interval = interval;
+        this.quantity = quantity;
+        this.patientId = patientId;
+
+    }
+
+
+
+
+}
+
