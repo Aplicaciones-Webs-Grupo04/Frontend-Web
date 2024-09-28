673137e4b28c4f40de5b7cbfe7866f6255f84732
Sihuar Ccotarma <Sihuarc1711@gmail.com>
2024-09-27 19:32:48 -0500

    add: component to sessions

---
+++ src/BC_Appinment_admin/components/SessionsListComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="session-card">
+    <h2 class="title">{{ session.patient.name }} {{ session.patient.lastName }}</h2>
+    <p class="subject"> Subject:{{session.asunto}}</p>
+    <p class="date">Date of appointment: {{ session.appointmentDate }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    session: Object
+  }
+}
+</script>
+
+<style>
+.session-card {
+  background-color: #f9f9f9;
+  border: 1px solid #ddd;
+  border-radius: 10px;
+  padding: 20px;
+  margin-bottom: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+</style>
