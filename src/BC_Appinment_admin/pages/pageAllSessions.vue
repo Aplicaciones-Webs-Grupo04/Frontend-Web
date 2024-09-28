05c07a9f3a5f32d8491420425cb16c7779245f9f
Sihuar Ccotarma <Sihuarc1711@gmail.com>
2024-09-27 23:07:19 -0500

    add:  Page showing all sessions is added.

---
+++ src/BC_Appinment_admin/pages/pageAllSessions.vue
@@ -0,0 +1,28 @@
+<script>
+import SessionListComponent from '../components/SessionListComponent.vue';
+export default {
+  components: { SessionListComponent }
+}
+</script>
+
+<template>
+<div class="headerPage">   <h1 class="titlePage">list of sessions</h1> </div>
+  <SessionListComponent />
+</template>
+
+<style>
+.headerPage {
+  background-color: #091133;
+  color: #fff;
+  padding: 1em;
+  text-align: center;
+}
+.titlePage {
+  text-align: center;
+  font-size: 36px;
+  font-weight: bold;
+  color: white;
+  margin-bottom: 20px;
+}
+
+</style>
