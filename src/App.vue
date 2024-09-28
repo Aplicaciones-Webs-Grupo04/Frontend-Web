09c993880fee99cd8cf817d9918fa6bd877715d7
Fernando Lizano <167820462+GuardianDeity@users.noreply.github.com>
2024-09-28 04:56:48 -0500

    refactor: add icons and links for the dashboard

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,32 @@
+<template>
+  <pv-toast></pv-toast>
+  <header>
+    <pv-toolbar class="bg-primary" fixed>
+      <template #start>
+        <pv-button class="p-button-text text-white" icon="pi pi-bars" @click="toggleDrawer" />
+        <h3>PSYMED</h3>
+        <div class="flex-column">
+          <router-link v-for="item in items" :key="item.label" v-slot="{ navigate, href }" :to="item.to" custom>
+            <pv-button :href="href" class="p-button-text text-white" @click="navigate">{{ item.label }}</pv-button>
+          </router-link>
+        </div>
+      </template>
+    </pv-toolbar>
+  </header>
+  <pv-sidebar v-model:visible="drawer">
+    <div class="sidebar-icons">
+      <router-link v-for="icon in topIcons" :key="icon.label" v-slot="{ navigate, href }" :to="icon.to" custom>
+        <pv-button :icon="icon.icon" class="p-button-text icon-button" :href="href" @click="navigate" />
+      </router-link>
+      <div class="spacer"></div>
+      <router-link :to="loginIcon.to" v-slot="{ navigate, href }" custom>
+        <pv-button :icon="loginIcon.icon" class="p-button-text icon-button" :href="href" @click="navigate" />
+      </router-link>
+    </div>
+  </pv-sidebar>
+  <router-view/>
+</template>
+
 <script>
 export default {
   name: 'app',
@@ -8,11 +37,17 @@ export default {
       items: [
         { label: 'Home', to: '/home' },
         { label: 'About', to: '/about' },
-          //medication está aquí solo de prueba:)
         { label: 'Medication', to: '/medication' },
         { label: 'Apoinment Admin', to: '/sessions' },
         { label: 'Add Apoinment', to: '/addSession' },
-      ]
+      ],
+      topIcons: [
+        { label: 'Home', to: '/home', icon: 'pi pi-home' },
+        { label: 'Alerts', to: '/alerts', icon: 'pi pi-bell' },
+        { label: 'Profile', to: '/profile', icon: 'pi pi-user' },
+        { label: 'Tasks', to: '/tasks', icon: 'pi pi-list' },
+      ],
+      loginIcon: { label: 'Login', to: '/login', icon: 'pi pi-sign-in' }
     };
   },
   methods: {
@@ -22,36 +57,17 @@ export default {
   }
 }
 </script>
-<template>
-  <pv-toast></pv-toast>
-  <header>
-    <pv-toolbar class="bg-primary" fixed>
-      <template #start>
-        <pv-button class="p-button-text text-white" icon="pi pi-bars" @click="toggleDrawer" />
-        <h3>PSYMED</h3>
-        <div class="flex-column">
-          <router-link v-for="item in items" :key="item.label" v-slot="{ navigate, href}" :to="item.to" custom>
-            <pv-button :href="href" class="p-button-text text-white" @click="navigate">{{ item.label }}</pv-button>
-          </router-link>
-        </div>
-      </template>
-    </pv-toolbar>
-  </header>
-  <pv-sidebar v-model:visible="drawer"/>
-  <router-view/>
-</template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.sidebar-icons {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.spacer {
+  flex-grow: 1;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.icon-button .pi {
+  font-size: 24px; /* Ajusta el tamaño según tus necesidades */
 }
 </style>
