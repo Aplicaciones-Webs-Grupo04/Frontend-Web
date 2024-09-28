7b835d064d69e9ab8d1fdd37468cca7dff107785
Sihuar Ccotarma <Sihuarc1711@gmail.com>
2024-09-27 19:30:08 -0500

    add: add dependencies

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from './router';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+import 'primevue/resources/themes/mdc-light-indigo/theme.css';
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
