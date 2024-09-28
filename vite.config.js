06d10cc7e05db6941e26fa1a7fdfff3cd1e1e312
Fernando Lizano <167820462+GuardianDeity@users.noreply.github.com>
2024-09-28 10:42:04 -0500

    fix: fix all api references

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://alurageek-api-peach.vercel.app',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
+  build: {
+    chunkSizeWarningLimit: 1030, // Adjust the limit as needed
   }
 });
