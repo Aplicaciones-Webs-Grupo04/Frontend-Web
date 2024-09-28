9ff1d1b825c5394b793e761979f87e9476a1e718
Fernando Lizano <167820462+GuardianDeity@users.noreply.github.com>
2024-09-27 20:45:28 -0500

    refactor: update reference link for api service

--- src/shared/services/http-common.js
+++ src/shared/services/http-common.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = 'http://localhost:3000';
 
 const http = axios.create({
-    baseUrl: API_BASE_URL,
+    baseURL: API_BASE_URL,
     headers: { 'Content-type': 'application/json' }
 });
 
