c7b9269770d2050055c3977ad49361f990d36805
Sihuar Ccotarma <Sihuarc1711@gmail.com>
2024-09-27 23:09:37 -0500

    add: add router

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import {createRouter, createWebHistory} from 'vue-router';
-import allSessions from '../BC_Appinment_admin/pages/allSessions.vue';
-
+import allSessions from '../BC_Appinment_admin/pages/pageAllSessions.vue';
+import addSessions from '../BC_Appinment_admin/pages/PageAddSessions.vue';
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/sessions', component: allSessions, meta: { title: 'Sessions' } },
+        { path:'/addSession',component: addSessions, meta:{title: 'Add Session'}},
     ],
 });
 router.beforeEach((to, from, next) => {
