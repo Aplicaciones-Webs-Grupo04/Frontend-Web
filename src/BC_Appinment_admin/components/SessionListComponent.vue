1a66dd9269f8b352c2638f70a193386c729336c9
Sihuar Ccotarma <Sihuarc1711@gmail.com>
2024-09-27 23:05:14 -0500

    add: Session List Component is added

---
+++ src/BC_Appinment_admin/components/SessionListComponent.vue
@@ -0,0 +1,55 @@
+
+<script>
+import SessionCard from '../components/SessionsComponent.vue';
+import { obtenerSessions } from '../Service/session-api.js';
+import { Session } from '../entities/Session';
+
+export default {
+  components: { SessionCard },
+  data() {
+    return {
+      sessions: []
+    }
+  },
+  mounted() {
+    obtenerSessions()
+        .then(response => {
+          this.sessions = response.map(Session.fromJson);
+        })
+        .catch(error => {
+          console.error(error);
+        })
+  }
+}
+</script>
+<template>
+  <div class="body">
+    <ul class="ListCont">
+      <li class="list" v-for="session in sessions" :key="session.id">
+        <SessionCard :session="session" />
+      </li>
+    </ul>
+  </div>
+</template>
+<style>
+
+.body {
+  max-width: 100%;
+  margin: 20px auto;
+  padding: 10px;
+  background-color: #69BFB7;
+  border: none;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+.ListCont {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.list {
+  /* Add some margin to each list item */
+  margin-bottom: 20px;
+}
+</style>
