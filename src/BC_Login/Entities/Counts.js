2372f8658b145d8d7b1f04c1a80833d305a75a6b
Sihuar Ccotarma <Sihuarc1711@gmail.com>
2024-11-16 00:47:21 -0500

    Add: Add Counts.js Entity to BC_Login

---
+++ src/BC_Login/Entities/Counts.js
@@ -0,0 +1,9 @@
+
+export default class User {
+    constructor(id, userName, password, role) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.role = role;
+    }
+}
